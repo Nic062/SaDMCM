8b431ccfda98522ac9c65f52bea0961ddbe078fa
Nicolas <nicolasvarlet@outlook.fr>
2017-03-29 08:45:20 +0200

    Creation entité

---
+++ src/entites/Lecture.java
@@ -0,0 +1,20 @@
+package entites;
+
+public class Lecture {
+	
+	private String fichier;
+
+	public String getFichier() {
+		return fichier;
+	}
+
+	public void setFichier(String fichier) {
+		this.fichier = fichier;
+	}
+
+	public Lecture(String fichier) {
+		this.fichier = fichier;
+	}
+	
+
+}
--- src/entites/Objet.java
+++ src/entites/Objet.java
@@ -1,3 +1,4 @@
+package entites;
 
 public class Objet {
 	private float profit;
