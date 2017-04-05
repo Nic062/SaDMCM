989d6b86798ddc52804504887bd90ba9a3b7fe6e
Sangvin <sang2712@gmail.com>
2017-04-05 12:58:32 +0200

    Premier test de l'algorithme

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
+import java.util.ArrayList;
+
 import entites.Lecture;
+import entites.Sac;
 
 public class Main {
 	/**
@@ -8,19 +11,52 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		long debut = System.currentTimeMillis();
-		Lecture lecture = new Lecture("instances/I1.txt");
+		Lecture lecture = new Lecture("instances/I0.txt");
 		lecture.lireEntete();
 		lecture.lireContraintesCapacite();
 		for(int i=0; i<lecture.getSac().getNbGroupes();i++)
 			lecture.lireGroupe(i);
-		lecture.getSac().getNbGroupes();
+		Sac sac = lecture.getSac();
+		algorithme(sac);
 		for(int i=0; i<lecture.getSac().getNbGroupes();i++)
 			for(int j=0;j<lecture.getSac().getObjParGroupe();j++)
 				System.out.println(lecture.getSac().getObjet(i, j));
 		long fin = System.currentTimeMillis();
 		System.out.println("Temps d'éxecution : " + (fin - debut) + " ms");
 	}
-	
-	/* TODO: Mettre en place algo 1 */
+
+	public static void algorithme(Sac sac) {
+		ArrayList<Sac> listeSac = new ArrayList<Sac>();
+		
+		for (int i = 0; i < sac.getNbGroupes(); i++) {
+			sac.setChoisit(i, 0);
+		}
+
+		for (int i = 0; i < sac.getNbGroupes(); i++) {
+			for (int j = 0; j < sac.getObjParGroupe(); j++) {
+				sac.setChoisit(i, j);
+				listeSac.add(sac);
+			}
+		}
+
+		for (int i = 0; i < sac.getNbContraintes(); i++) {
+			for (Sac proposition : listeSac) {
+				if (!proposition.verifierContrainte(i)) {
+					listeSac.remove(proposition);
+				}
+			}
+		}
+		Sac meilleur = listeSac.get(0);
+		for (Sac proposition : listeSac) {
+			if (proposition.getProfit() > meilleur.getProfit()) {
+				meilleur = proposition;
+			}
+		}
+		
+		System.out.println("lol");
+		for(int i=0; i<meilleur.getNbGroupes();i++)
+			for(int j=0;j<meilleur.getObjParGroupe();j++)
+				System.out.println(meilleur.getObjet(i, j));
+	}
 
 }
