75b64ffab8408044f77974cb476428a71486e751
Sangvin <sang2712@gmail.com>
2017-05-02 11:50:06 +0200

    Debut algo

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,6 @@ package main;
 
 import entites.Combinaison;
 import entites.Lecture;
-import entites.Objet;
 import entites.Sac;
 
 public class Main {
@@ -27,10 +26,46 @@ public class Main {
 	}
 
 	public static void algorithme() {
-		combin2(0, sac.getListObjet(), null);
+		sac.trierCroissant();
+		
+		// Mettre combinaison de depart !
+		lastBestCombinaison = new Combinaison();
+		for (int i = 0; i < sac.getNbGroupes(); i++) {
+			lastBestCombinaison.getListeObjet().add(sac.getObjet(i, 0));
+		}
+		
+		// verifier contrainte
+		for (int j = 0; j < sac.getNbContraintes(); j++) {
+    		if (sac.verifierContrainte(j, lastBestCombinaison)){
+    			System.out.println("Contrainte " + (j + 1) + " verifiée");
+    		}
+    		else
+    		{
+    			System.out.println("Contrainte " + (j + 1) + " non verifiée");
+    		}
+    	}
+		
 		print(lastBestCombinaison);
 		System.out.println("Profit = "+lastBestCombinaison.getProfit());
 		
+	}
+	
+	public static int scalaire(int[] coef, int[] contraintes) {
+		int res = 0;
+		for (int i = 0; i < coef.length; i++) {
+			res += coef[i] * contraintes[i];
+		}
+		return res;
+	}
+	
+	public static double norme(int [] vecteur) {
+		double res = 0;
+		for (int i = 0; i < vecteur.length; i++) {
+			res += Math.pow(vecteur[i], 2.0);
+		}
+		
+		res = Math.sqrt(res);	
+		return res;
 	}
 
 	public static void print(Combinaison combinaison) {
@@ -38,44 +73,4 @@ public class Main {
 			System.out.println(combinaison.getListeObjet().get(obj));
 		}
 	}
-	
-	public static void combin2(int depth, Objet[][]matrix, Objet[] output){
-        Objet[] row = matrix[depth];
-
-        if(depth == 0) {
-            counter = 0;
-            output = new Objet[matrix.length];
-            System.out.println("Taille de la matrice: " + matrix.length);
-        }
-
-        for(int i=0; i<row.length; i++) {
-            output[depth] = row[i];
-
-            if(depth == (matrix.length-1)) {
-                
-            	Combinaison comb = new Combinaison();
-            	for (Objet objet : output) {
-					comb.getListeObjet().add(objet);
-				}
-            	
-            	boolean ajout = true;
-            	for(int j=0;j<sac.getNbContraintes();j++){
-            		if(!sac.verifierContrainte(j, comb)){
-            			ajout = false;
-            			break;
-            		}
-            	}
-            	if(ajout){
-            		if(lastBestCombinaison==null || lastBestCombinaison.getProfit()<comb.getProfit()){
-            			lastBestCombinaison = comb;
-            		}
-            	}
-            	
-                counter++;
-            } else {
-                combin2(depth+1, matrix, output);
-            }
-        }
-    }
-
 }
